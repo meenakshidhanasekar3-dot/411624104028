766aa36e19de83fe7f47dc21f18bf021c4b9f300
meena2050 <meenakshidhanasekar3@gmail.com>
2026-06-16 11:15:01 +0530

    JS

---
+++ demo1.js
@@ -0,0 +1,29 @@
+// console.log("A")
+// console.log("B")
+// console.log("C")
+// const promise=new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         console.log("Hi")
+//     }, 2000)
+//     setTimeout(()=>{
+//         console.log("Done")
+//     }, 0)
+//     console.log("D")
+// })
+// promise.then(res=>{console.log(res)})
+// .catch(err=>{console.log(err)})
+
+
+console.log(1)
+console.log(2)
+console.log("Hi")
+function example(){
+    console.log("Hello")
+}
+const examples=()=>{
+    const a=1;
+    const b=2;
+    console.log(1+2)
+}
+setTimeout(example,3000)
+setTimeout(examples,450)
